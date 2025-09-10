c2768abcbc75b48c1c792020cb87aea0749a7bbc
Maino Cheng <yesdmlc@gmail.com>
2025-09-09 19:05:03 -0700

    Add custom date filter for Nunjucks

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,10 @@
+const { DateTime } = require("luxon");
+
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addFilter("date", (dateObj, format = "MMMM d, yyyy") => {
+    return DateTime.fromJSDate(dateObj).toFormat(format);
+  });
+
   return {
     dir: {
       input: "src",
