53f265379fd4a161344d2d6f335acb961128793d
Maino Cheng <yesdmlc@gmail.com>
2025-09-09 19:20:32 -0700

    Add passthrough copy for image uploads

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,13 @@
 const { DateTime } = require("luxon");
 
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("src/images/uploads");
+
   eleventyConfig.addFilter("date", (dateObj, format = "MMMM d, yyyy") => {
     return DateTime.fromJSDate(dateObj).toFormat(format);
   });
+
+  eleventyConfig.addGlobalData("now", () => new Date());
 
   return {
     dir: {
